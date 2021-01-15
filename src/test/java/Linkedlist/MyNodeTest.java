49783936cfd68b5595801ebe87cd294feb13b40f
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-16 01:17:50 +0530

    Added:Three Integer To Linked List

---
+++ src/test/java/Linkedlist/MyNodeTest.java
@@ -0,0 +1,19 @@
+package Linkedlist;
+
+import org.junit.Assert;
+
+public class MyNodeTest {
+	public void given3NumbersWhenLinkedShouldPassToLinkedList() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+
+}
+
