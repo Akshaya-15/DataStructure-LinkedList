221726b3b566aded34785f92f26df4243630611a
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-17 00:11:34 +0530

    Three Integer Append Bottom Of Linked List

--- src/test/java/Linkedlist/MyLinkedListTest.java
+++ src/test/java/Linkedlist/MyLinkedListTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 public class MyLinkedListTest {
 	 @Test
 	    public void given3NumbersWhenAddedToLinkedListShouldAddedToTop() {
-	        INode<Integer> myFirstNode = new INode<>(70);
-	        INode<Integer> MySecondNode =new INode<>(30);
-	        INode<Integer> myThirdNode = new INode<>(56);
+	        MyNode<Integer> myFirstNode = new MyNode<>(70);
+	        MyNode<Integer> MySecondNode =new MyNode<>(30);
+	        MyNode<Integer> myThirdNode = new MyNode<>(56);
 	        MyLinkedList myLinkedList = new MyLinkedList();
 	        myLinkedList.add(myFirstNode);
 	        myLinkedList.add(MySecondNode);
@@ -20,5 +20,23 @@ public class MyLinkedListTest {
 	        Assert.assertTrue(result);
 
 	    }
+	 @Test
+	    public void given3NumbersWhenAppendedToLinkedListShouldAddedToLast() {
+	        MyNode<Integer> myFirstNode = new MyNode<>(56);
+	        MyNode<Integer> mySecondNode = new MyNode<>(30);
+	        MyNode<Integer> myThirdNode = new MyNode<>(70);
+	        MyLinkedList myLinkedList = new  MyLinkedList();
+	        myLinkedList.add(myFirstNode);
+	        myLinkedList.append(mySecondNode);
+	        myLinkedList.append(myThirdNode);
+
+	        boolean result = myLinkedList.head.equals(myFirstNode) &&
+	                myLinkedList.head.getNext().equals(mySecondNode) &&
+	                myLinkedList.tail.equals(myThirdNode);
+	        Assert.assertTrue(result);
+
+	    }
+
 	}
+	
 
